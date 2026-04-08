daef0ac9596863f59b114bdbc3087a59257ac9f4
nhen <jenapoian@gmail.com>
2026-04-08 15:26:39 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,64 +1,33 @@
-const CACHE_NAME = 'nwc-v1.2';
+const CACHE_NAME = 'nwc-v1.3';
 const ASSETS = [
     './',
     './index.html',
     './manifest.json',
     'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
     'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+    'https://unpkg.com/leaflet.heat/dist/leaflet-heat.js',
     'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
     'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
     'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png'
 ];
 
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(ASSETS);
-        })
-    );
+    event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cache) => {
-                    if (cache !== CACHE_NAME) {
-                        return caches.delete(cache);
-                    }
-                })
-            );
-        })
-    );
+    event.waitUntil(caches.keys().then((names) => Promise.all(names.map((c) => { if (c !== CACHE_NAME) return caches.delete(c); }))));
 });
 
 self.addEventListener('fetch', (event) => {
     const url = event.request.url;
-
-    // Trap Carto tiles, Esri Satellite tiles, and manifest icons for offline use
-    if (url.includes('cartocdn.com') || url.includes('arcgisonline.com') || url.includes('githubusercontent.com') || url.includes('iconify.design')) {
-        event.respondWith(
-            caches.match(event.request).then(res => 
-                res || fetch(event.request).then(netRes => 
-                    caches.open(CACHE_NAME).then(cache => { 
-                        cache.put(event.request, netRes.clone()); 
-                        return netRes; 
-                    })
-                )
-            )
-        );
+    if (url.includes('cartocdn.com') || url.includes('arcgisonline.com') || url.includes('githubusercontent.com') || url.includes('iconify.design') || url.includes('unpkg.com')) {
+        event.respondWith(caches.match(event.request).then(res => res || fetch(event.request).then(netRes => caches.open(CACHE_NAME).then(cache => { cache.put(event.request, netRes.clone()); return netRes; }))));
         return; 
     }
-
     if (url.includes('index.html') || url === self.location.origin + '/') {
-        event.respondWith(
-            fetch(event.request).catch(() => caches.match(event.request))
-        );
+        event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
     } else {
-        event.respondWith(
-            caches.match(event.request).then((response) => {
-                return response || fetch(event.request);
-            })
-        );
+        event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
     }
 });
